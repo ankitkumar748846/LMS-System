fc9412f6b017ec1b052b84747b2b2a80e0f884d0
ankit <ankitkumar748846@gmail.com>
2024-02-14 01:48:18 +0530

    added react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 );
